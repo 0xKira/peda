05b52bc246919b6cd484431a016b7200dd4a2be4
cMc_SARS <l.dmxcsnsbh@gmail.com>
2018-08-31 13:33:01 +0000

    Update peda.py

--- peda.py
+++ peda.py
@@ -7245,7 +7245,7 @@ Alias("reg", "peda xinfo register")
 peda.execute("set confirm off")
 peda.execute("set verbose off")
 peda.execute("set output-radix 0x10")
-peda.execute("set prompt \001%s\002" % red("\002gdb-peda$ \001","light")) # custom prompt
+peda.execute("set prompt \001%s\002" % red("\002gdb-peda$ \001")) # custom prompt
 peda.execute("set height 0") # disable paging
 peda.execute("set history expansion on")
 peda.execute("set history save on") # enable history saving
